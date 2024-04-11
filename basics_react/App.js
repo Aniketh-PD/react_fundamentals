e4b6343c569366becde9f7f79a029dea213031f4
Aniketh Deokar <anikethdeokar@Anikeths-MacBook-Air.local>
2024-04-11 18:15:05 +0530

    cdn removed and installed react react-dom from npm

--- basics_react/App.js
+++ basics_react/App.js
@@ -1,18 +1,6 @@
-/**
- *
- * <div id = "parent">
- *      <div id="child1">
- *          <h1>Hi I am h1 tag</h1>
- *          <h2>Hi I am h2 tag</h2>
- *     <div>
- *     <div id="child2">
- *          <h1>Hi I am h1 tag <h1>
- *          <h2>Hi I am h2 tag <h2>
- *      <div>
- * </div>
- *
- *
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "I am h1 tag"),
