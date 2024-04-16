d9772689b53d775f51105c7b7b8b32ff656c1dcb
Aniketh Deokar <anikethdeokar@Anikeths-MacBook-Air.local>
2024-04-16 16:07:38 +0530

    completed search functionality for page

--- basics_react/src/components/Body.js
+++ basics_react/src/components/Body.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
-import resData from "../utils/mockData";
 
 const Body = () => {
+  const [filteredRestaurantList, setFilteredRestaurantList] = useState([]);
   const [restaurantList, setRestaurantList] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -15,7 +16,11 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.960059122809971&lng=77.57337538383284&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const jsonData = await data.json();
-    console.log(jsonData);
+
+    setFilteredRestaurantList(
+      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
     setRestaurantList(
       jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants
@@ -23,10 +28,21 @@ const Body = () => {
   };
 
   const handleClick = () => {
-    const filteredRestaurantList = restaurantList.filter(
+    const filteredRestaurantListOnRating = filteredRestaurantList.filter(
       (restaurant) => restaurant.info.avgRating > 4
     );
-    setRestaurantList(filteredRestaurantList);
+    setFilteredRestaurantList(filteredRestaurantListOnRating);
+  };
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const handleSearchClick = () => {
+    const restaurantsMatchingSearch = restaurantList.filter((restaurant) => {
+      return restaurant?.info?.name?.toLowerCase()?.includes(searchValue?.toLowerCase());
+    });
+    setFilteredRestaurantList(restaurantsMatchingSearch);
   };
 
   if (restaurantList.length === 0) {
@@ -35,11 +51,15 @@ const Body = () => {
 
   return (
     <div className="body">
-      <div className="filter-btn">
+      <div className="filter">
+        <div className="search-container">
+          <input type="text" value={searchValue} onChange={handleChange} />
+          <button onClick={handleSearchClick}>Search</button>
+        </div>
         <button onClick={handleClick}>Top Rated Restaurants</button>
       </div>
       <div className="restaraunt-container">
-        {restaurantList.map((restaurant) => (
+        {filteredRestaurantList.map((restaurant) => (
           <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
         ))}
       </div>
