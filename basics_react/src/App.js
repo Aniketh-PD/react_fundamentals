edbd21c9af0a02652dac97b253118b283c0b4c69
Aniketh Deokar <anikethdeokar@Anikeths-MacBook-Air.local>
2024-04-16 17:01:24 +0530

    added routing to app

--- basics_react/src/App.js
+++ basics_react/src/App.js
@@ -2,69 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
-
-
-const resObj = {
-  info: {
-    id: "10892",
-    name: "Pizza Hut",
-    cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
-    locality: "Vani Vilas Road",
-    areaName: "Basavanagudi",
-    costForTwo: "₹350 for two",
-    cuisines: ["Pizzas"],
-    avgRating: 4,
-    parentId: "721",
-    avgRatingString: "4.0",
-    totalRatingsString: "5K+",
-    sla: {
-      deliveryTime: 38,
-      lastMileTravel: 1.9,
-      serviceability: "SERVICEABLE",
-      slaString: "35-40 mins",
-      lastMileTravelString: "1.9 km",
-      iconType: "ICON_TYPE_EMPTY",
-    },
-    availability: {
-      nextCloseTime: "2024-04-21 00:00:00",
-      opened: true,
-    },
-    badges: {},
-    isOpen: true,
-    type: "F",
-    badgesV2: {
-      entityBadges: {
-        imageBased: {},
-        textBased: {},
-        textExtendedBadges: {},
-      },
-    },
-    aggregatedDiscountInfoV3: {
-      header: "ITEMS",
-      subHeader: "AT ₹179",
-    },
-    differentiatedUi: {
-      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
-      differentiatedUiMediaDetails: {
-        mediaType: "ADS_MEDIA_ENUM_IMAGE",
-        lottie: {},
-        video: {},
-      },
-    },
-    reviewsSummary: {},
-    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
-    restaurantOfferPresentationInfo: {},
-  },
-  analytics: {},
-  cta: {
-    link: "https://www.swiggy.com/restaurants/pizza-hut-vani-vilas-road-basavanagudi-bangalore-10892",
-    type: "WEBLINK",
-  },
-};
-
-
-
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
   return (
@@ -75,5 +16,20 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },{
+    path : '/contact',
+    element: <Contact />
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter}/>);
